e8d853ea3a4db4ccbfe0ed90907d8c0f2c92e4ec
Prokop Randáček <prokop.randacek@gmail.com>
2018-09-14 08:43:09 +0200

    Jidlo se centruje do boxu. Osetren resize bug

--- js.js
+++ js.js
@@ -1,23 +1,35 @@
-var zindex = 1
+var zindex = 1;
 var IDs = ["hruska", "banan"];
+var destroy = 0;
 
-function resize() {
-	IDs.forEach(element => {
-		dragElement(document.getElementById(element));
-	});
+function init() {
+	console.log("initing")
+	destroy = 0
+	for (i = 0; i < IDs.length; i++) {
+		dragElement(document.getElementById(IDs[i]));
+	}
+}
+
+function reinit() {
+	console.log("reiniting")
+	destroy = IDs.length;
+	init();
 }
 
 function dragElement(elmnt) {
 	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-	var rect = elmnt.getBoundingClientRect();
-	var startposX = rect.left;
-	var startposY = rect.top;
-	var ebox = document.getElementById(elmnt.id+"box")
-	var box = ebox.getBoundingClientRect();
+	var elmntRect = elmnt.getBoundingClientRect();
+	var startposX = elmntRect.left;
+	var startposY = elmntRect.top;
+	var box = document.getElementById(elmnt.id+"box")
+	var boxRect = box.getBoundingClientRect();
 	var vratse = true;
-	ebox.style.backgroundColor = "red";
-	elmnt.onmousedown = dragMouseDown;
+	var pageHeight = window.innerHeight;
+	var pageWidth = window.innerWidth;
 	
+	box.style.backgroundColor = "red";
+	elmnt.onmousedown = dragMouseDown;
+
 	function dragMouseDown(e) {
 		elmnt.style.zIndex = (zindex + 1).toString();
 		zindex += 1;
@@ -27,6 +39,10 @@ function dragElement(elmnt) {
 		pos4 = e.clientY;
 		document.onmouseup = closeDragElement;
 		document.onmousemove = elementDrag;
+		if (destroy > 0) {
+			remove();
+			destroy -= 1;
+		}
 	}
 	function elementDrag(e) {
 		e = e || window.event;
@@ -37,57 +53,54 @@ function dragElement(elmnt) {
 		pos3 = e.clientX;
 		pos4 = e.clientY;
 
-		var offsets = elmnt.getBoundingClientRect();
-		var top = offsets.top;
-		var left = offsets.left;
-		var bottom = offsets.bottom;
-		var right = offsets.right;
-		var pageHeight = window.innerHeight;
-		var pageWidth = window.innerWidth;
+		if (elmntRect.top > -1 && elmntRect.bottom < pageHeight + 1) { //v
+			elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+		}
+		else if (elmntRect.bottom > (pageHeight)) { //pod
+			elmnt.style.top = (pageHeight - elmnt.offsetHeight) + "px";
+		}
+		else if (elmntRect.top < 0) { //nad
+			elmnt.style.top = 0 + "px";
+		}
 
-		if (top > -1 && bottom < pageHeight + 1) //v
-		{ elmnt.style.top = (elmnt.offsetTop - pos2) + "px"; }
-		else if (bottom > (pageHeight)) //pod
-		{ elmnt.style.top = (pageHeight - elmnt.offsetHeight) + "px"; }
-		else if (top < 0) //nad
-		{ elmnt.style.top = 0 + "px"; }
+		if (elmntRect.left > -1 && elmntRect.right < pageWidth + 1) { //v
+			elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+		}
+		else if (elmntRect.right > pageWidth) { //vpravo
+			elmnt.style.left = (pageWidth - elmnt.offsetWidth) + "px";
+		}
+		else if (elmntRect.left < 0) { //vlevo
+			elmnt.style.left = 0 + "px";
+		}
 
-		if (left > -1 && right < pageWidth + 1) //v
-		{ elmnt.style.left = (elmnt.offsetLeft - pos1) + "px"; }
-		else if (right > pageWidth) //vpravo
-		{ elmnt.style.left = (pageWidth - elmnt.offsetWidth) + "px"; }
-		else if (left < 0) //vlevo
-		{ elmnt.style.left = 0 + "px"; }
-
-		vboxu(e)
+		boxCheck(e);
 	}
-	function vboxu(e) {
-		if (e.clientX > box.left  && e.clientY > box.top &&
-			e.clientX < box.right && e.clientY < box.bottom) { //uvnitr
-			document.getElementById(elmnt.id + "uvnitr").textContent = elmnt.id + " uvnitr=true";
-			ebox.style.backgroundColor = "green";
-			vratse = false
+	function boxCheck(e) {
+		if (e.clientX > boxRect.left  && e.clientY > boxRect.top &&
+			e.clientX < boxRect.right && e.clientY < boxRect.bottom) { //uvnitr
+			document.getElementById(elmnt.id + "uvnitr").textContent = elmnt.id + " uvnitr=true"; //debug pise do divu
+			box.style.backgroundColor = "green";
+			vratse = false;
 		} else { //mimo
-			document.getElementById(elmnt.id + "uvnitr").textContent = elmnt.id + " uvnitr=false";
-			ebox.style.backgroundColor = "red";
-			vratse = true
+			document.getElementById(elmnt.id + "uvnitr").textContent = elmnt.id + " uvnitr=false"; //debug pise do divu
+			box.style.backgroundColor = "red";
+			vratse = true;
 		}
 	}
 	function closeDragElement() {
 		if (vratse) {
-			translate(elmnt, startposX, startposY)
+			translate(startposX, startposY);
+		}
+		else {
+			translate(((boxRect.left + boxRect.right) / 2) - (elmnt.offsetWidth / 2), (boxRect.top + boxRect.bottom) / 2 - (elmnt.offsetHeight / 2));
 		}
 		document.onmouseup = null;
 		document.onmousemove = null;
 	}
-	function drop() {
-		translate(elmnt, startposX, startposY)
-		document.onmouseup = null;
-		document.onmousemove = null;
-	}
-	function translate(elem, x, y) {
-		var left = parseInt(window.getComputedStyle(elem, null).getPropertyValue('left'), 10);
-		var top = parseInt(window.getComputedStyle(elem, null).getPropertyValue('top'), 10);
+	function translate(x, y) {
+		console.log(elmnt.id, x, y);
+		var left = parseInt(window.getComputedStyle(elmnt, null).getPropertyValue('left'), 10);
+		var top = parseInt(window.getComputedStyle(elmnt, null).getPropertyValue('top'), 10);
 		var dx = left - x;
 		var dy = top - y;
 		var i = 1;
@@ -97,14 +110,15 @@ function dragElement(elmnt) {
 		function loop() {
 			if (i >= count) { return; }
 			i += 1;
-			elem.style.left = (left - (dx * i / count)).toFixed(0) + 'px';
-			elem.style.top = (top - (dy * i / count)).toFixed(0) + 'px';
+			elmnt.style.left = (left - (dx * i / count)).toFixed(0) + 'px';
+			elmnt.style.top = (top - (dy * i / count)).toFixed(0) + 'px';
 			setTimeout(loop, delay);
 		}
 		loop();
 	}
+	function remove() {
+		document.onmouseup = null;
+		document.onmousemove = null;
+		elmnt.onmousedown = null;
+	}
 }
-
-document.mouseleave(function () {
-    alert()
-});
